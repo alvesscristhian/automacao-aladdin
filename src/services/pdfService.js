764f27334e066d3b291070566dc4db22afc57eb7
Cristhian Alves <cristhian.ads@hotmail.com>
2026-08-22 15:49:58 -0300

    feat: PDF generate with value order

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -2,6 +2,32 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const path = require('path');
 
+function groupByValue(items) {
+    const groups = new Map();
+
+    items.forEach((item) => {
+        if (!groups.has(item.value)) {
+            groups.set(item.value, []);
+        }
+
+        groups.get(item.value).push(item);
+    });
+
+    return groups;
+}
+
+function writeGroupedItems(doc, items) {
+    groupByValue(items).forEach((group, value) => {
+        doc.text(`Valor: R$ ${value.toFixed(2)}`);
+
+        group.forEach((item) => {
+            doc.text(`  ${item.number}`);
+        });
+
+        doc.moveDown(0.5);
+    });
+}
+
 module.exports = function generatePDF(data) {
     return new Promise((resolve, reject) => {
         const doc = new PDFDocument();
@@ -25,18 +51,12 @@ module.exports = function generatePDF(data) {
 
         doc.fontSize(14);
         doc.text('CENTENA');
-
-        data.centena.forEach((item) => {
-            doc.text(`${item.number} - R$ ${item.value.toFixed(2)}`);
-        });
+        writeGroupedItems(doc, data.centena);
 
         doc.moveDown();
 
         doc.text('MILHAR');
-
-        data.milhar.forEach((item) => {
-            doc.text(`${item.number} - R$ ${item.value.toFixed(2)}`);
-        });
+        writeGroupedItems(doc, data.milhar);
 
         doc.moveDown();
 
